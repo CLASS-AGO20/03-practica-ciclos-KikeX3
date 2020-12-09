968f20759b877c1f0dbdf77fc3b9c537656a1b16
KikeX3 <jguerrero24@ucol.mx>
2020-12-09 16:29:55 -0600

    Implented Factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+        let factorial = 1;
+
+        for (let i = numero ; i > 0 ; i = i - 1 ){
+            
+            factorial = factorial * i;
+
+        }
+        return factorial
+    }
+
+    convertirAstring(numero){
+
+    }
+
+    obtenerDivisibles(numero){
+
+    }
+
+    elevar(numero,potencia){
+
+    }
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
