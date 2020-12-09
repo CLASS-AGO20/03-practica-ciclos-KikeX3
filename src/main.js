e49fc54a3a3209d1b58573ef26c49e2797fe084e
KikeX3 <jguerrero24@ucol.mx>
2020-12-09 16:57:58 -0600

    implementes obtenerDivisibles y convertirAstring

--- src/main.js
+++ src/main.js
@@ -12,10 +12,31 @@ export default class App {
     }
 
     convertirAstring(numero){
+        let string = "";
+        let i = 1;
 
+        while(i <= numero){
+            string = string + "*";
+            i = i + 1;
+        }
+        return string;
     }
 
     obtenerDivisibles(numero){
+
+        let divisibles = 0;
+        let i = numero;
+
+        do{
+
+            if(numero%i === 0){
+                divisibles = divisibles + 1;
+            }
+
+            i = i - 1;
+        }while(i>0);
+       
+        return divisibles;
 
     }
 
@@ -27,3 +48,7 @@ export default class App {
 let app = new App();
 
 console.log(app.factorial(5));
+
+console.log(app.convertirAstring(5));
+
+console.log(app.obtenerDivisibles(6));
